d189d81f90d62d53da338131697b71c22a4cf425
wenchyuan333 <wenchyuan333@gmail.com>
2026-08-29 01:57:18 +0800

    ci: isolate q3e A5

--- verify_all.py
+++ verify_all.py
@@ -1,9 +1,9 @@
-"""Temporary q3e test-level bisect: A4."""
+"""Temporary q3e test-level bisect: A5."""
 import runpy
 import sys
 
 module = runpy.run_path("q3-luoshu/rh_proof_audit.py", run_name="q3e_probe")
-name = "test_F3_axioms"
+name = "test_klein_four_group_of_functional_equation"
 try:
     module[name]()
     print("PASS", name)
